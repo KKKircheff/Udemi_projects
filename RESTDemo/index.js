2899ab825bd1f9d6f0eb3af58eda912e0a25c67a
KKKircheff <kircheff@protonmail.com>
2022-10-03 11:09:33 +0200

    Adding comment section

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended:true})) // tells Express to parse information coming from URL encoded request body
 app.use(express.json()); // tells express to parse data coming from json file
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
 
+//***************************/
+// faking a database
+let comments = [
+    {
+        
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+// end faking database
+//********************/
+
+
+app.get('/comment', (req,res) => {
+    res.render('comments/index', {comments}); 
+    // line above renders index.ejs and passes the data as an object to the rendered tepmplate 
+})
 
 app.post('/tacos', (req,res) => {
     const {meat, qty} = req.body;
